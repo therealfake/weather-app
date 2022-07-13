4cd3d5f463435817bee84b0b012f36eb49bfa4c8
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-07-12 20:27:43 -0400

    add weather fetching with openweathermap api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.scss'
+
+async function getWeatherInfo(city) {
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9ea7f4cb3800cc6901c2fe0669ad0aac`;
+        const response = await fetch(url, {mode: 'cors'});
+        const data = await response.json();
+        const desiredData = {
+            location: data.name + ', ' + data.sys.country,
+            description: data.weather[0].description,
+            temp: data.main.temp,
+            temp_feels_like: data.main.feels_like,
+            wind_speed: data.wind.speed,
+            humidity: data.main.humidity
+        }
+        return desiredData
+    } catch (error) {
+        console.log('error');
+    }
+}
+getWeatherInfo('toronto').then(data => console.log(data));
